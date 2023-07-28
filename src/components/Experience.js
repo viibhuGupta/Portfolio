2695e9384f9bd4c916517fb3025edd7a5d27d481
Vibhu Gupta <vik6026717961@gmail.com>
2023-07-29 01:00:51 +0530

    commit

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
+import { motion, useScroll } from "framer-motion";
 
+
+const Details = ({position , company , companyLink , time , address ,work}) =>{
+
+  return <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-center  ">
+    <div>
+      <h3  className=" capotalize font-bold text-2xl   ">{position}  &nbsp; <a href={companyLink} 
+      target={"_blank"}
+      className="text-primary capitalize cursor-pointer"
+      >@{company}</a></h3>
+      <span className=" capotalize font-medium text-dark/75   ">
+             {time} | {address}
+      </span>
+      <p  className="font-medium w-full ">
+          {work}
+      </p>
+    </div>
+  </li>
+
+}
 export const Experience = () => {
   return (
-    <div className='my-64 '>
-        <h2 className='font-bold text-8xl mb-32 w-full text-center'>
+    <div className="my-64 ">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center">
         Experience
-        </h2>
-        </div>
-  )
-}
+      </h2>
+
+      <div className="w-[75%] mx-auto relative  ">
+
+
+       <div className=" absolute left-8 top-0 w-[4px] h-full bg-dark origin-top    "/>
+
+        <ul className="w-full flex flex-col items-start justify-between ml-4   ">
+
+        <Details 
+        
+        position={"Software Engineer"}
+        company={"Google"}
+        companyLink={"https://www.google.com/"}
+        time={"2022-Present"}
+        address={"Mountain View, CA"}
+        work={"Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and  developing new tools for data analysis and visualization."}
+
+        />
+
+
+<Details 
+        
+        position={"Software Engineer"}
+        company={"Google"}
+        companyLink={"https://www.google.com/"}
+        time={"2022-Present"}
+        address={"Mountain View, CA"}
+        work={"Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and  developing new tools for data analysis and visualization."}
+
+        />
+
+
+
+<Details 
+        
+        position={"Software Engineer"}
+        company={"Google"}
+        companyLink={"https://www.google.com/"}
+        time={"2022-Present"}
+        address={"Mountain View, CA"}
+        work={"Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and  developing new tools for data analysis and visualization."}
+
+        />
+
+
+
+
+<Details 
+        
+        position={"Software Engineer"}
+        company={"Google"}
+        companyLink={"https://www.google.com/"}
+        time={"2022-Present"}
+        address={"Mountain View, CA"}
+        work={"Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and  developing new tools for data analysis and visualization."}
+
+        />
+
+
+
+<Details 
+        
+        position={"Software Engineer"}
+        company={"Google"}
+        companyLink={"https://www.google.com/"}
+        time={"2022-Present"}
+        address={"Mountain View, CA"}
+        work={"Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and  developing new tools for data analysis and visualization."}
+
+        />
+
+
+
+
+        </ul>
+
+        
+        
+      </div>
+
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { Layout } from '@/components/Layout';
 import Head from 'next/head';
-import profilePic from "../../public/images/profile/developer-pic-1.png";
+import profilePic from "../../public/images/profile/developer-pic-1.gif";
 import Image from 'next/image';
 import { AnimatedText } from '@/components/AnimatedText';
 import Link from 'next/link';
@@ -19,8 +19,8 @@ export default function Home() {
       <main className=" flex items-center text-dark  w-full  min-h-screen ">
         <Layout className='pt-0'>
           <div className='flex items-center justify-between w-full'>
-            <div className='w-1/2 ml-[40px]'> 
-           <Image src={profilePic} alt='Vibhu' className= " w-full h-auto"  />
+            <div className='w-1/2 ml-[40px] '> 
+           <Image src={profilePic} alt='Vibhu' className= " w-[80%] h-auto"  />
 
             </div>
             <div className='w-1/2 flex flex-col items-center self-center mr-[40px]'>
